6c7fc4785a199bb363e56b5623bf7f43e419ed0b
leduongnhattt <nhatnhatnhatle@gmail.com>
2024-08-08 22:51:57 +0700

    do login and register API

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,16 +1,56 @@
 
-import { registerSchema } from './../_helpers/validators/index';
+import { loginSchema, registerSchema } from './../_helpers/validators/index';
 import { Request, Response } from "express";
+import bcrypt from 'bcrypt';
+import { UserModel } from '../db/user';
 
 class AuthController {
   public async login(request: Request, respone: Response) {
-    return respone.json({message: 'login'})
+    try {
+      const { email, password} = request.body;
+
+      loginSchema.parse({ email, password });
+
+      const user = await UserModel.findOne({
+        email,
+      });
+
+      if(user) {
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if(isPasswordMatch) {
+          return respone
+            .status(201)
+            .json({ message: 'User logged in successfully', data: user})
+        }
+      }
+      return respone
+        .status(400)
+        .json({ status: false, message: 'Invalid credentials' })
+    }
+    catch(error){
+      return respone.status(400).json({ status: false, error})
+    }
   }
   public async register(request: Request, respone: Response) {
     try {
       const { name, email, password } = request.body;
 
       registerSchema.parse({ name, email, password });
+
+      const saltRounds = 10;
+      const salt = await bcrypt.genSalt(saltRounds);
+      const hashPassword = await bcrypt.hash(password, salt);
+
+      const user = new UserModel({
+        name,
+        email,
+        password: hashPassword,
+      });
+      await user.save();
+
+      return respone
+        .status(201)
+        .json({ message: 'User created successfully' })
     }
     catch(error) {
       return respone.status(400).json({ status: false, error });
