c7eec89442f7e0219e4e47bb5597f2ec7363585f
leduongnhattt <nhatnhatnhatle@gmail.com>
2024-08-15 15:14:49 +0700

    handle compose messages

--- src/app/pages/compose/compose.component.ts
+++ src/app/pages/compose/compose.component.ts
@@ -1,15 +1,66 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit } from '@angular/core';
 import { CardComponent } from '../../shared/component/card/card.component';
 import { SidebarComponent } from '../../shared/layouts/sidebar/sidebar.component';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { GetAllUser, GetLoggedInUser, UserState } from '../../store/User.State';
+import { Observable } from 'rxjs';
+import { IUser } from '../../core/models/auth.model';
+import { Select, Store } from '@ngxs/store';
+import { CommonModule } from '@angular/common';
+import { GetAllMessage } from '../../store/MessageState';
+import { MessageService } from '../../core/services/message.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-compose',
   standalone: true,
-  imports: [CardComponent, SidebarComponent],
+  imports: [CardComponent, SidebarComponent, ReactiveFormsModule, CommonModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './compose.component.html',
   styleUrl: './compose.component.scss'
 })
-export class ComposeComponent {
+export class ComposeComponent implements OnInit{
+
+    @Select(UserState.getLoggedUser) user$ !: Observable<IUser>;
+    @Select(UserState.selectUsers) users$ !: Observable<IUser[]>;
+    formBuilder = inject(FormBuilder);
+    messageService: MessageService = inject(MessageService);
+    toastr: ToastrService = inject(ToastrService);
+    store:Store = inject(Store);
+    composeForm: FormGroup;
+
+    constructor() {
+      this.composeForm = this.formBuilder.group({
+        to: new FormControl('', [Validators.required]),
+        sender: new FormControl('', [Validators.required]),
+        subject: new FormControl('', [Validators.required]),
+        body: new FormControl('', [Validators.required]),
+      });
+    }
+  ngOnInit(): void {
+    this.user$.subscribe({
+      next: (user) => {
+        this.composeForm.patchValue({
+          sender: user?._id,
+        });
+        if(!user) {
+          this.store.dispatch(new GetLoggedInUser());
+        }
+      }
+    })
+    this.store.dispatch(new GetAllUser());
+  }
+
+  sentMessage() {
+    if(this.composeForm.valid) {
+      this.messageService.sentMessage(this.composeForm.value).subscribe({
+        next: (response) => {
+          this.store.dispatch(new GetAllMessage());
+          this.toastr.success(response.message);
+        }
+      })
+    }
+  }
+
 
 }
--- src/app/shared/layouts/header/header.component.ts
+++ src/app/shared/layouts/header/header.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
 import { Observable } from 'rxjs';
 import { TokenService } from '../../../core/services/token.service';
 import { CommonModule } from '@angular/common';
+import { GetLoggedInUser, UserState } from '../../../store/User.State';
+import { IUser } from '../../../core/models/auth.model';
+import { Select, Store } from '@ngxs/store';
 
 @Component({
   selector: 'app-header',
@@ -12,10 +15,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+
+  @Select(UserState.getLoggedUser) user$ !: Observable<IUser>;
+  store: Store = inject(Store);
   isAuthenticate$!: Observable<boolean>;
   constructor(private authService: AuthService, private tokenService: TokenService){
     this.isAuthenticate$ = this.tokenService.isAuthentication;
+  }
+  ngOnInit(): void {
+    this.user$.subscribe({
+      next: (user) => {
+        if(!user) {
+          this.store.dispatch(new GetLoggedInUser());
+        }
+      }
+    })
   }
 
   onLogout() {
